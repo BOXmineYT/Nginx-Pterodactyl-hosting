a69df171c8f93853716aaeb600e5f57d06c11450
BOXmine <131708727+BOXmineYT@users.noreply.github.com>
2024-10-05 13:12:23 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -6,12 +6,12 @@ rm -rf /home/container/tmp/*
 # [Setup] Load the PHP version from the file
 PHP_VERSION=$(cat "/home/container/php_version.txt")
 
-# [Docker] Starting PHP-FPM with the specified PHP version
-echo "[Docker] Starting PHP-FPM"
+# [BOX-Hosting.ru] Starting PHP-FPM with the specified PHP version
+echo "[BOX-Hosting.ru] Starting PHP-FPM"
 php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
 
-# [Docker] Starting NGINX
-echo "[Docker] Starting NGINX"
-echo "[Docker] Services successfully launched"
+# [BOX-Hosting.ru] Starting NGINX
+echo "[BOX-Hosting.ru] Starting NGINX"
+echo "[BOX-Hosting.ru] Services successfully launched"
 
 nginx -c /home/container/nginx/nginx.conf -p /home/container
