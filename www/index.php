d6e0c51b310517810f321f51871e14a0b3dd9ab2
BOXmine <131708727+BOXmineYT@users.noreply.github.com>
2024-10-05 13:06:40 +0300

    Version 1

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site BOX-Hosting.ru</title>
+    <link rel="stylesheet" href="https://web.box-hosting.ru/indexweb.css">
+	<link rel="icon" href="https://web.box-hosting.ru/icon.png">
+</head>
+<body>
+    <div class="container">
+        <div class="black-block">
+            <img src="https://web.box-hosting.ru/logoweb.png" alt="BOX-Hosting.ru" class="logo">
+            <h1>The site was created on BOX-Hosting.ru</h1>
+            <a href="https://box-hosting.ru" class="button">Go over</a>
+        </div>
+    </div>
+</body>
+</html>
